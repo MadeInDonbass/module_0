ab1c33c604c876568426880b65c8e0cf8b9f98fb
MadeInDonbass <khalaim.nastya@gmail.com>
2021-08-04 23:33:59 +0300

    i was planned to somehow remove extra numbers from the list, but i was failed ¯\_('')_/¯

--- GuessNumber.py
+++ GuessNumber.py
@@ -6,3 +6,11 @@ def game_core_v2(number):
     list_numbers = [number for num in range(1, 101)]
     predict = list_numbers[49]
     mx, mn = list_numbers[-1], list_numbers[0]
+    while number != predict:
+        count += 1
+        if number > predict:
+            mn = predict
+        elif number < predict:
+            mx = predict + 1
+        predict = mn + (mx - mn) // 2
+        return count
