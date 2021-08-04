22dd95155780c42984454141970dfe33b54dcd2a
MadeInDonbass <khalaim.nastya@gmail.com>
2021-08-04 23:38:21 +0300

    to the original code add spases and delete extra parenthesis

--- GuessNumber.py
+++ GuessNumber.py
@@ -14,3 +14,17 @@ def game_core_v2(number):
             mx = predict + 1
         predict = mn + (mx - mn) // 2
         return count
+
+
+def score_game(game_core):
+    count_ls = []
+    np.random.seed(1)
+    random_array = np.random.randint(1, 101, size=1000)
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return score
+
+
+score_game(game_core_v2)
